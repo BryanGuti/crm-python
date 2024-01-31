5fe1bc7b7e337ad3f0c1b3eb36860efb31af6d3a
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-01-30 23:35:22 -0400

    Create models for user register

--- crm_web/models.py
+++ crm_web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    address = models.CharField(max_length=60)
+    city = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")

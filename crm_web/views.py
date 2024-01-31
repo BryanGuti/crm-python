59714f7596e129b7f4c496bd6b67b842cab284a4
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-01-31 03:57:13 -0400

    Add customer table at home page

--- crm_web/views.py
+++ crm_web/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -20,7 +23,7 @@ def home(request):
                 request, 'There was an error logging in. Try it again.')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 # def login_user(request):
@@ -48,3 +51,12 @@ def register_user(request):
     else:
         form = SignUpForm
         return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be logged in!')
+        return redirect('home')
